650a8500826841681cf9d752697cf9d9ee1fa98a
Mihai Dogaru <dogarumihai2537@gmail.com>
2025-09-24 21:20:43 +0300

    Add html skel

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Hello world!");
+
+const customInputEl = document.getElementById("inputCustomTip");
+
+customInputEl.addEventListener("click", () => {
+  document.querySelector('input[value="custom"]').checked = true;
+});
